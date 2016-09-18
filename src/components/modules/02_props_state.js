114475da8b7662299216b012da5b648699f8dfc4
choijaewoong <jwchoi0129@gmail.com>
2016-09-18 16:18:54 +0900

    propTypes 예제 * 참고 : https://facebook.github.io/react/docs/reusable-components.html

--- src/components/modules/02_props_state.js
+++ src/components/modules/02_props_state.js
@@ -7,6 +7,7 @@ class Props extends React.Component {
       <div>
         <h1> Hello {this.props.name}</h1>
         <p>{this.props.children}</p>
+        <p>{this.props.number}</p>
       </div>
     );
   }
@@ -18,7 +19,7 @@ class PropsState extends React.Component {
         <Props name="React">&#39;this.props.children&#39;는 Element안의 내용을 가지게 됩니다.</Props>
         {/* Router를 쓸경우 default 값을 설정 하지 않는 이상
             이런 식의 문법을 쓸 수 없어 보임 */}
-        <Props name={this.props.name}>{this.props.value}</Props>
+        <Props name={this.props.name} number={this.props.number}>{this.props.value}</Props>
       </div>
     );
   }
@@ -26,7 +27,13 @@ class PropsState extends React.Component {
 // PropsState의 기본값 설정
 PropsState.defaultProps = {
   name: "Props World",
+  number: 5,
   value: "기본 값을 설정 할 수 있습니다."
+}
+// PropsState props의 타입 설정
+PropsState.propTypes = {
+  name : React.PropTypes.string,
+  number: React.PropTypes.number.isRequired
 }
 
 export default PropsState
